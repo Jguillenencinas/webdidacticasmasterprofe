18b9c73c42e2a051ad1cdafb5b849e4a54c3ab6d
PONDERATO <jonas.carasa@gmail.com>
2024-05-16 19:37:59 +0200

    Cambios en inicio y contenidos

--- contenido.js
+++ contenido.js
@@ -19,4 +19,20 @@ listElements.forEach(listElement => {
 function changeVideo(videoId) {
     const videoFrame = document.getElementById('video-frame');
     videoFrame.src = `https://www.youtube.com/embed/${videoId}`;
+    videoFrame.style.opacity = 100;
+}
+
+function changePic(picId) {
+    const picFrame = document.getElementById('pic-frame');
+    picFrame.src = `./img/${picId}`;
+    picFrame.style.opacity = 100;
+    picFrame.style.width = picFrame.src.style.width;
+    picFrame.style.height = picFrame.src.style.height;
+}
+
+function changeLink(linkId) {
+    const linkFrame = document.getElementById('link-frame');
+    const linkDiv = document.getElementById('link-div');
+    linkFrame.href = `https://${linkId}`;
+    linkDiv.style.opacity = 100;
 }
